39117b2a8b65a7f4da75c578ac02aae9eb2cfc91
Jindrich Dolezy <jindrich@dolezy.cz>
2014-03-13 15:21:25 +0100

    Fixed activity indicator property

--- PopoverView/R20PopoverView+ActivityIndicator.m
+++ PopoverView/R20PopoverView+ActivityIndicator.m
@@ -2,12 +2,21 @@
 // Created by Jindrich Dolezy on 13.03.14.
 //
 
+#import <objc/runtime.h>
 #import "R20PopoverView+ActivityIndicator.h"
 
 
 @implementation R20PopoverView (ActivityIndicator)
 
 #pragma mark - Activity Indicator
+
+- (UIActivityIndicatorView *)activityIndicator {
+    return objc_getAssociatedObject(self, @selector(activityIndicator));
+}
+
+- (void)setActivityIndicator:(UIActivityIndicatorView *)activityIndicator {
+    objc_setAssociatedObject(self, @selector(activityIndicator), activityIndicator, OBJC_ASSOCIATION_ASSIGN);
+}
 
 //Animates in a progress indicator, and removes
 - (void)showActivityIndicatorWithMessage:(NSString *)msg {
@@ -16,6 +25,7 @@
     [self hideAllSubviewsWithDuration:0.2f];
 
     [self.activityIndicator removeFromSuperview];
+    self.activityIndicator = nil;
 
     // self.activityIndicator is weak, so we have to store it in the strong variable before adding to superview
     UIActivityIndicatorView *ai = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
@@ -35,6 +45,7 @@
         self.activityIndicator.alpha = 0.f;
     } completion:^(BOOL finished) {
         [self.activityIndicator removeFromSuperview];
+        self.activityIndicator = nil;
     }];
 }
 
